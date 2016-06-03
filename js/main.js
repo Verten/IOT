a154c44675a110051f963155626cff92481fb8f1
ebinhon <bin.hong@ericsson.com>
2016-06-03 09:33:48 +0800

    commit with new page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 $(function(){
-    //Device Info
-    $('.ebTable-contentColumn').click(function(){
-        $('.elLayouts-Wrapper').addClass('elLayouts-Wrapper-showInfo');
-        $('.elLayouts-deviceInfo').addClass('elLayouts-deviceInfo-show');
-    });
-    $('.ebLayout-HeadingCommands').click(function(){
-        $('.elLayouts-Wrapper').removeClass('elLayouts-Wrapper-showInfo');
-        $('.elLayouts-deviceInfo').removeClass('elLayouts-deviceInfo-show');
-    });
-
-    //Table
-    $('.ebTableRow').click(function(){
-        $(this).addClass('ebTableRow_highlighted').siblings(".ebTableRow").removeClass('ebTableRow_highlighted');
-    });
- });
+				//Device Info
+				$('.ebTable-contentColumn').click(function(){
+			 	    $('.elLayouts-Wrapper').addClass('elLayouts-Wrapper-showInfo');
+			 	    $('.elLayouts-deviceInfo').addClass('elLayouts-deviceInfo-show');
+			 	});
+				$('.ebLayout-HeadingCommands').click(function(){
+			 	    $('.elLayouts-Wrapper').removeClass('elLayouts-Wrapper-showInfo');
+			 	    $('.elLayouts-deviceInfo').removeClass('elLayouts-deviceInfo-show');
+			 	});
+			 	
+			 	//Table
+			 	$('.ebTableRow').click(function(){
+			 	    $(this).addClass('ebTableRow_highlighted').siblings(".ebTableRow").removeClass('ebTableRow_highlighted');			 	   
+			 	});
+			 	
+			 	//Select
+			 	$('.ebDropdown-header').click(function(){
+			 	    $(this).next('.ebComponentList').css('display','block');
+			 	});
+			 	
+			 	//Add Custom Attribute
+			 	$('.addAttributesButton').click(function(){
+			 		
+			 	    $(this).next('.elLayouts-addAttributes').show(200);
+			 	    $('.addAttributesButton').hide();
+			 	    
+			 	});
+			 	
+			 	//Display Custom Attribute
+			 	$('.ebLayout-SectionCustomAttributes .ebLayout-SectionTitle').click(function(){
+			 		
+			 	    $(this).next('.ebLayout-formSection').toggle(200);
+			 	    $(this).children(".ebIcon").toggleClass('ebIcon_upArrow_10px');
+			 	    
+			 	});
+			 })
